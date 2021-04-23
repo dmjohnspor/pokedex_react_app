b3beb528e9b0e6234086ece5e0b5899dcff0472d
dmjohnspor <ioannis.sporidis@gmail.com>
2021-04-23 17:55:20 -0400

    Fixed search input with toLowerCase method

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,142 +1,153 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
-    AppBar,
-    Toolbar,
-    Grid,
-    Card,
-    CardMedia,
-    CardContent,
-    CircularProgress,
-    Typography,
-    TextField,
-    Button
-} from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import { toFirstCharUppercase } from '../utils';
+  AppBar,
+  Toolbar,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  CircularProgress,
+  Typography,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import { toFirstCharUppercase } from "../utils";
 
-const useStyles = makeStyles(theme => ({
-    pokedexContainer: {
-        paddingTop: '20px',
-        paddingLeft: '50px',
-        paddingRight: '50px'
-    },
-    cardMedia: {
-        margin: 'auto',
-    },
-    cardContent: {
-        textAlign: 'center',
-    },
-    searchContainer: {
-        display: 'flex',
-        background: fade(theme.palette.common.white, 0.15),
-        paddingLeft: '20px',
-        paddingRight: '20px',
-        marginTop: '5px',
-        marginBottom: '5px'
-    },
-    searchIcon: {
-        alignSelf: 'flex-end',
-        marginBottom: '5px',
-    },
-    searchInput: {
-        width: '200px',
-        margin: '5px',
-    },
-    button: {
-        background: 'red',
-        color: 'white'
-    }
+const useStyles = makeStyles((theme) => ({
+  pokedexContainer: {
+    paddingTop: "20px",
+    paddingLeft: "50px",
+    paddingRight: "50px",
+  },
+  cardMedia: {
+    margin: "auto",
+  },
+  cardContent: {
+    textAlign: "center",
+  },
+  searchContainer: {
+    display: "flex",
+    background: fade(theme.palette.common.white, 0.15),
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    marginTop: "5px",
+    marginBottom: "5px",
+  },
+  searchIcon: {
+    alignSelf: "flex-end",
+    marginBottom: "5px",
+  },
+  searchInput: {
+    width: "200px",
+    margin: "5px",
+  },
+  button: {
+    background: "red",
+    color: "white",
+  },
 }));
 
 const Pokedex = (props) => {
-    const { history } = props;
-    const classes = useStyles();
-    const [pokemonData, setPokemonData] = useState({});
-    const [filter, setFilter] = useState('');
+  const { history } = props;
+  const classes = useStyles();
+  const [pokemonData, setPokemonData] = useState({});
+  const [filter, setFilter] = useState("");
 
-    const handleSearchChange = (e) => {
-        setFilter(e.target.value);
-    };
+  const handleSearchChange = (e) => {
+    let input = e.target.value;
+    setFilter(input.toLowerCase());
+  };
 
-    useEffect(() => {
-        axios
-            .get(`https://pokeapi.co/api/v2/pokemon?limit=898`)
-            .then(function (response) {
-                console.log(response)
-                const { data } = response;
-                const { results } = data;
-                const newPokemonData = {};
-                results.forEach((pokemon, index) => {
-                    newPokemonData[index + 1] = {
-                        id: index + 1,
-                        name: pokemon.name,
-                        sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
-                    };
-                });
-                setPokemonData(newPokemonData);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=898`)
+      .then(function (response) {
+        console.log(response);
+        const { data } = response;
+        const { results } = data;
+        const newPokemonData = {};
+        results.forEach((pokemon, index) => {
+          newPokemonData[index + 1] = {
+            id: index + 1,
+            name: pokemon.name,
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              index + 1
+            }.png`,
+          };
+        });
+        setPokemonData(newPokemonData);
+      });
+  }, []);
 
-    const getPokemonCard = (pokemonId) => {
-        const { id, name, sprite } = pokemonData[pokemonId];
-
-        return (
-            <Grid item xs={12} sm={4} key={pokemonId}>
-                <Card onClick={() => history.push(`/${pokemonId}`)}>
-                    <CardMedia
-                        className={classes.cardMedia}
-                        image={sprite}
-                        style={{ width: '130px', height: '130px' }}
-                    />
-                    <CardContent className={classes.cardContent}>
-                        <Typography>
-                            {`${id}. ${toFirstCharUppercase(name)}`}
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Grid>
-        );
-    };
+  const getPokemonCard = (pokemonId) => {
+    const { id, name, sprite } = pokemonData[pokemonId];
 
     return (
-        <>
-            <AppBar position='static'>
-                <Toolbar>
-                    <Grid container direction='row' justify='flex-start' alignItems='center' spacing={3}>
-                        <Grid item>
-                            <div className={classes.searchContainer}>
-                                <SearchIcon className={classes.searchIcon} />
-                                <TextField
-                                    className={classes.searchInput}
-                                    label='Pokemon'
-                                    variant='standard'
-                                    onChange={handleSearchChange}
-                                />
-                            </div>
-                        </Grid>
-                        <Grid item>
-                            <Button
-                                className={classes.button}
-                                variant="contained"
-                                onClick={() => history.push('/')}
-                            >
-                                Back to Pokedex selection
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </Toolbar>
-            </AppBar>
-            {pokemonData ? (
-                <Grid container spacing={2} className={classes.pokedexContainer}>
-                    {Object.keys(pokemonData).map(pokemonId =>
-                        pokemonData[pokemonId].name.includes(filter) &&
-                        getPokemonCard(pokemonId))}
-                </Grid>
-            ) : (<CircularProgress />)}
-        </>
+      <Grid item xs={12} sm={4} key={pokemonId}>
+        <Card onClick={() => history.push(`/${pokemonId}`)}>
+          <CardMedia
+            className={classes.cardMedia}
+            image={sprite}
+            style={{ width: "130px", height: "130px" }}
+          />
+          <CardContent className={classes.cardContent}>
+            <Typography>{`${id}. ${toFirstCharUppercase(name)}`}</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
     );
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="center"
+            spacing={3}
+          >
+            <Grid item>
+              <div className={classes.searchContainer}>
+                <SearchIcon className={classes.searchIcon} />
+                <TextField
+                  className={classes.searchInput}
+                  label="Pokemon"
+                  variant="standard"
+                  onChange={handleSearchChange}
+                />
+              </div>
+            </Grid>
+            <Grid item>
+              <Button
+                className={classes.button}
+                variant="contained"
+                onClick={() => history.push("/")}
+              >
+                Back to Pokedex selection
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      {pokemonData ? (
+        <Grid container spacing={2} className={classes.pokedexContainer}>
+          {Object.keys(pokemonData).map(
+            (pokemonId) =>
+              pokemonData[pokemonId].name.includes(filter) &&
+              getPokemonCard(pokemonId)
+          )}
+        </Grid>
+      ) : (
+        <CircularProgress />
+      )}
+    </>
+  );
 };
 
 export default Pokedex;
